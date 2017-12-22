ef79d4b69be04742894e8d98bba459bb036ae436
Sharona Young <youngs_e@hotmail.com>
2017-12-22 11:43:05 -0500

    added ajax calls

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,3 +1,24 @@
 $(document).ready(function () {
 
-};
+// google places API: AIzaSyDvoVUjY-466T_MG7ZUxYXxXzmF6MJusCY
+
+var queryURLrest="https://maps.googleapis.com/maps/api/place/textsearch/xml?query=mall+of+america&type=restaurant&key=AIzaSyDvoVUjY-466T_MG7ZUxYXxXzmF6MJusCY"
+
+queryURLlodg="https://maps.googleapis.com/maps/api/place/textsearch/xml?query=mall+of+america&type=lodging&key=AIzaSyDvoVUjY-466T_MG7ZUxYXxXzmF6MJusCY"
+
+$.ajax({
+	url: queryURLrest,
+	method: "GET"
+}).done(function(response) {
+	console.log(response);
+});
+
+$.ajax({
+	url: queryURLlodg,
+	method: "GET"
+}).done(function(response) {
+	console.log(response);
+});
+
+
+});
